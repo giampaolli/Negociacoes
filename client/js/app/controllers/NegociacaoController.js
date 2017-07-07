9b538c81b0e80a5dbc5b78433cbe53a504aba67e
giampaolli <felipegiampaoli@yahoo.com.br>
2017-07-07 09:38:11 -0300

    começando novamente

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -3,35 +3,32 @@ class NegociacaoController {
     constructor() {
         
         let $ = document.querySelector.bind(document);
+        
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-
-        this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(),
-            new NegociacoesView($('#negociacoesView')),
-            'adiciona', 'esvazia');
          
+        this._listaNegociacoes = new Bind(
+            new ListaNegociacoes(), 
+            new NegociacoesView($('#negociacoesView')), 
+            'adiciona', 'esvazia');
+       
         this._mensagem = new Bind(
-            new Mensagem(),
-            new MensagemView($('#mensagemView')),
-            'texto');        
+            new Mensagem(), new MensagemView($('#mensagemView')),
+            'texto');       
     }
     
     adiciona(event) {
         
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        
-        this._mensagem.texto = 'Negociação adicionada com sucesso';
-        
+        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
         this._limpaFormulario();   
     }
     
     apaga() {
         
         this._listaNegociacoes.esvazia();
-         
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
     
